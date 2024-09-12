0c6b4d9889be26584a2399b5c1470344dd3d3188
Ioan Krumov <ioankrumov@gmail.com>
2024-09-12 10:45:08 +0300

    Initial project and architecture

---
+++ GlobalFirePower/GlobalFirePower/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+using GlobalFirePower.Views;
+
+namespace GlobalFirePower
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenMenuPage();
+        }
+
+        private void OpenMenuPage()
+        {
+            Menu pg = new Menu();
+            this.Content = pg;
+        }
+    }
+}
